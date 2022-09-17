1e43c8e200f4e976fe78fdea0b6fc1daf8de8206
Maria <maria.seminar@ya.ru>
2022-09-17 20:23:03 +0300

    add additional verification

--- day00/ex04/counter.sh
+++ day00/ex04/counter.sh
@@ -6,10 +6,10 @@ else
   my_path=$1
 fi
 echo '"name","count"' > hh_uniq_positions.csv
-
-cat $my_path \
+cat ${my_path} \
   | awk 'BEGIN{FS=OFS=",";} NR>1 {print $3;}' \
   | sort \
   | uniq -c \
+  | sort -t ',' -r -k 2 \
   | awk 'BEGIN{OFS=","} {print $2, $1;}' \
   >>  hh_uniq_positions.csv
