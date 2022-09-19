d0d91491a984616b3fc35b6eda8f37b5085ebda2
Maria <maria.seminar@ya.ru>
2022-09-19 12:23:45 +0300

    change d01 ex01

--- day01/ex01/read_and_write.py
+++ day01/ex01/read_and_write.py
@@ -1,13 +1,12 @@
 def read_and_write() -> None:
-    file_r = open("ds.csv", "r")
-    file_w = open("ds.tsv", "w")
-    text = file_r.read() \
-        .replace("\",", "\"\t") \
-        .replace(",\"", "\t\"") \
-        .replace("true,", "true\t") \
-        .replace("false,", "false\t")
-    file_w.write(text)
-    file_r.close()
+    res = ''
+    file_w = open("ds.tsv", 'w')
+    with open("ds.csv", 'r') as file:
+        for line in file:
+            res = line.replace('false,', 'false\t')
+            res = res.replace('true,', 'true\t')
+            res = res.replace("\",", "\"\t")
+            file_w.write(res)
     file_w.close()
 
 if __name__ == "__main__":
