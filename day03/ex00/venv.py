227d41542d2e2b7df77a53a25e4169e436fd8b30
Maria <maria.seminar@ya.ru>
2022-09-20 03:01:42 +0300

    d03ex00

---
+++ day03/ex00/venv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import os
+
+def print_virtual_env():
+    print("Your current virtual env is " + os.environ['VIRTUAL_ENV'])
+
+if __name__ == "__main__":
+    print_virtual_env()
